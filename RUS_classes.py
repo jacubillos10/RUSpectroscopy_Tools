13c938cf58c30c1c473438c49e1fe463ae9d7ae9
Manuela Rivas <m.rivas2@uniandes.edu.co>
2024-02-12 20:50:40 -0500

    Began forward class and defined the methods needed to unify tha three classes and solve the forward problem

--- RUS_classes.py
+++ RUS_classes.py
@@ -7,7 +7,7 @@ class base:
 
     Attributes: 
      N <int>: Integer that represents maximum order in the base funcions.
-     phi <np.array<tuples<int>>>: Numpy array with the expansion of the base functions represented with indexes in tuples.
+     phi <np.array<tuples<int>>>: Numpy array with the expansion of the base functions represented with indexes in numpy arrays.
      type <string>: Name of the base functions. For example 'Legendre'.
 
     Methods: 
@@ -15,11 +15,13 @@ class base:
      calc_phi: Calculate the phi array based on the order N.
      get_type: Returns the user the name of the base functions that are being used.
      get_N: Returns the user the maximum order in the base funcions.
+     get_phi: Returns the user the 
     """
 
     def __init__(self,N,type):
         """
         Class initialization
+
         @Input:
          N <int>: Max order of the base function.
          type <str>: Name of the base functions.
@@ -27,6 +29,8 @@ class base:
         self.N = N
         self.type = type 
         self.phi = self.calc_phi(self)
+
+        return self
 
     def get_type(self):
         """
@@ -39,17 +43,26 @@ class base:
         N attribute class getter.
         """
         return self.N
+    
+    def get_phi(self):
+        """
+        phi attribute class getter.
+        """
+        return self.phi
 
     def calc_phi(self):
         """
         Method incharge of calculating the phi array made of the index combinations for the specified N.
+
         @Output:
          phi<np.array<tuples<int>>>:Numpy array with the expansion of the base functions represented with indexes in tuples.
         """
-        nums = np.arange(0, self.N+1)
-        i, j, k = np.broadcast_arrays(nums[:, None, None], nums[None, :, None], nums[None, None, :])        filtered = indices[np.sum(indices, axis=1) <= self.N]
-        indices_filtrados = np.where(i + j + k <= self.N)
+        N = self.N
+        nums = np.arange(0, N+1)
+        i, j, k = np.broadcast_arrays(nums[:, None, None], nums[None, :, None], nums[None, None, :])        
+        indices_filtrados = np.where(i + j + k <= N)
         phi = np.vstack([i[indices_filtrados], j[indices_filtrados], k[indices_filtrados]]).T
+        
         return phi
 
     
@@ -62,25 +75,38 @@ class sample:
      shape <str>: A string with the name of the shape of the sample. For example "cilinder" or "rectangular paralelepiped".
      system <str>: A string with the name of the shape of the sample. For example "orthorombic" or "isotropic".
      limits <np.array<int>>: An array with all the information about the size of the sample. 
+     rho <float>: Density of the sample.
+     C <np.array<np.array<float>>>: Constant matrix of the sample.
+
+
 
     Methods: 
-     __init__: Initialization method. shape, system and limits must be specified.
+     __init__: Initialization method. shape, system, rho, C and limits must be specified.
      get_shape: Returns the user the name of the shape of the sample.
      get_system: Returns the user the name of the crystalline system of teh sample.
      get_limits: Returns the user the array with the limits (size) of the sample.
+     get_rho: Returns the user the float that corresponds to the density of the sample.
+     get_C: Returns the user the constant matrix that corresponds to the sample.
+
     """
 
-    def __init__(self,shape,system,limits):
+    def __init__(self,shape,system,limits,rho,C):
         """
         Class initialization
+
         @Input:
          shape <str>: A string with the name of the shape of the sample.
          system <str>: A string with the name of the shape of the sample.
          limits <np.array<int>>: An array with all the information about the size of the sample. 
+         rho <float>: A float that corresponds to the density of the sample.
         """
         self.shape = shape
         self.system = system
         self.limits = limits
+        self.rho = rho
+        self.C = C
+
+        return self
     
     def get_shape(self):
         """
@@ -99,3 +125,16 @@ class sample:
         Limits attribute class getter.
         """
         return self.limits
+    
+    def get_rho(self):
+        """
+        Rho attribute class getter.
+        """
+        return self.rho
+    
+    def get_c(self):
+        """
+        C attribute class getter.
+        """
+        return self.C
+    
