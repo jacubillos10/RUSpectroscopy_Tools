5270e36c91391dd6886f276e140e79def60f4b0d
jacubillos10 <alejandro4cm@outlook.com>
2024-02-29 10:58:29 -0500

    Se terminó de documentar las funcion que genera un elemento matricial. Se tuvo problemas para generar la función que genera la matriz. numba no permite que hayan listas dentro de las funciones a compilar. También jode mucho si alguno de los np.array() que está dentro de la función no tiene el tipo de datos que espera. Lo que se hará es crear un np.array repleto de ceros al principio, y los ciclos for se encargarán de llenar esos ceros, y listo

--- rusmodules/compile_rus_module.py
+++ rusmodules/compile_rus_module.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import njit
+from numba import njit, types
 from numba.pycc import CC
 
 cc = CC('rus')
@@ -48,6 +48,7 @@ def generate_term_in_ſ(exp_index1, exp_index2, i1, i2, j1, j2, C, geo_par):
     array_j2[j2] = 1
     coeff = exp_index1 + exp_index2 + 1 - array_j1 - array_j2
     Q = (1 - (-1)**coeff[0])*(1 - (-1)**coeff[1])*(1 - (-1)**coeff[2])
+    #geo_par = np.array(geo_par, dtype = float)
     if Q == 0:
         return 0
     #fin if 
@@ -56,8 +57,20 @@ def generate_term_in_ſ(exp_index1, exp_index2, i1, i2, j1, j2, C, geo_par):
     return P*Q*R
 #fin función    
 
+@njit("f8(i8,i8,i8[:],i8[:],f8[:,:],f8[:])")
 @cc.export("generate_matrix_element_gamma", "f8(i8,i8,i8[:],i8[:],f8[:,:],f8[:])")
 def generate_matrix_element_ſ(i1, i2, exp_index1, exp_index2, C, geo_par):
+    """
+    This function generates a matrix element of ſ, given two axis indexes (0 is x, 1 is y, 2 is z), two arrays of indexes which
+    contain the exponents of the power series, the elastic constants and the geometric parameters
+    @Input i1 <int>: index i of the expresion in eq 11. 0 to x, 1 to y, 2 to z
+    @Input i2 <int>: index i' of the expresion in eq 11. 0 to x, 1 to y, 2 to z
+    @Input exp_index1 <np.array>: Exponents lambda, mu and nu of the p element in the matrix and i1 index
+    @Input exp_index2 <np.array>: Exponents lambda, mu and nu of the q element in the matrix and i2 index
+    @Input C <np.array>: Matrix with the elastic constants
+    @Input geo_par <np.array>: Geometric parameters of the sample, such as Lx, Ly, Lz. Be sure to enter a array of floats NOT INTEGERS!
+    @Return <float>: Returns a matrix element ſ
+    """
     acum = 0
     for j1 in range(3):
         for j2 in range(3):
@@ -66,6 +79,18 @@ def generate_matrix_element_ſ(i1, i2, exp_index1, exp_index2, C, geo_par):
     #fin for
     return acum
 #fin función
+
+#@njit("f8[:](i8, f8[:,:], f8[:])")
+@cc.export("gamma_matrix", "f8[:](i8, f8[:,:], f8[:])")
+def gamma_matrix(N, C, geo_par):
+    combi = np.zeros(3)
+    for i in range(N):
+        for j in range(N - j):
+            k = (N -1) - i - j
+            
+
+    #fin for 
+    return np.array([1.1,2.2,3.3])
 
 if __name__ == "__main__":
     cc.compile()
