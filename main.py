9c3a9b9fc99c5b0064f5ee28524aabc127a49fa5
jacubillos10 <alejandro4cm@outlook.com>
2024-03-21 12:50:17 -0500

    An eigenvalue problem has been tested with SrTiO3 constants reported in Migliori 1993. The answer is almost satisfactory because the eigenvalues result possitive. But there is a little headache: scipy is returning np.complex128 type values. Even if the imaginary part of each eigenvalue is 0, scipy is still reporting that imaginary part

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import numpy as np
 import rusmodules
 from rusmodules import rus
+import scipy
 
-C_const = np.genfromtxt('constantes.csv', delimiter=',', skip_header=0, dtype=float)
+C_const = np.genfromtxt('constantes2.csv', delimiter=',', skip_header=0, dtype=float)
 geometry = np.array([1.0,1.0,1.0])
 
 gamma = rus.gamma_matrix(1, C_const, geometry)
 E = rus.E_matrix(1)
-print(gamma)
-print("***************")
-print(E)
+
+vals, vects = scipy.linalg.eig(a = gamma, b = E)
+
+print(vals)
+print(vals[0])
+print(vals[0].imag)
