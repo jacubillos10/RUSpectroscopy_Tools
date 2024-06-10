d895815740614ce36c8f2120a9a5f8e38c3e9cb2
jacubillos10 <alejandro4cm@outlook.com>
2024-06-10 14:36:53 -0500

    Checkpoint1: A comparison has been made between our code and RUScal using FeGa, SmB6 and URu2Si2 parallelepiped samples. Starting from the seventh frequency all the frequencies calculated by the program are the same than the frequencies calculated with RUScal. The first 6th frequencies in our program are 0 in all samples. I can say that the parallelepiped forward problem runs well

--- main.py
+++ main.py
@@ -5,13 +5,30 @@ import scipy
 import matplotlib.pyplot as plt
 import time
 
+"""
+#Datos del FeGa
 Ng = 12
 rho = 7.979 #g/cm^3
 nombre_archivo = 'constantesFeGa.csv'
+"""
+
+"""
+#Datos del SmB6
+Ng = 16
+rho = 4.869 #g/cm^3
+nombre_archivo = 'constantesSmB6.csv' #Mbar
+"""
+
+#Datos del URu2Si2
+Ng = 14
+rho = 9.839 #g/cm^3
+nombre_archivo = 'constantesURu2Si2.csv' #Mbar
+
 
 C_const = np.genfromtxt(nombre_archivo, delimiter=',', skip_header=0, dtype=float)
-geometry = np.array([0.30529,0.20353,0.25334]) #cm
-
+#geometry = np.array([0.30529,0.20353,0.25334]) #cm  FeGa
+#geometry = np.array([0.10872, 0.13981, 0.01757]) #cm SmB6
+geometry = np.array([0.29605, 0.31034, 0.29138]) #cm URu2Si2
 gamma = rus.gamma_matrix(Ng, C_const, geometry)
 E = rus.E_matrix(Ng)
 
