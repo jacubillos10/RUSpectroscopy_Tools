39d73e5c6bf287729f37b4759443dd8a4cd37088
jacubillos10 <alejandro4cm@outlook.com>
2024-06-27 12:08:57 -0500

    The code that replicates the figure 4 and figure 5 of the Visscher paper now works well. The plots have been generated

--- replicar_graficas.py
+++ replicar_graficas.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 import time
 
 np.set_printoptions(suppress = True)
-shape = 2 # 0: parallelepiped, 1: cilinder, 2: ellipsoid 
+shape = 1 # 0: parallelepiped, 1: cilinder, 2: ellipsoid 
 alphas = (1, np.pi/4, np.pi/6)
 alpha = alphas[shape]
-Finura = 500
+Finura = 100
 
 A = 2
 #Datos del URu2Si2
 Ng = 8
-m = (A**3)*0.2634 #g 9.84029 #9.839 #g/cm^3 
-nombre_archivo = 'constantesURu2Si2.csv' #Mbar
-C_const = np.genfromtxt(nombre_archivo, delimiter=',', skip_header=0, dtype=float)
-initial_geometry = A*np.ones(3) #cm URu2Si2
-vol = alpha*np.prod(initial_geometry)
+rho = 1
+mat_2 = np.zeros((6,6))
+mat_3 = np.ones((3,3)) - np.identity(3)
+mat_2[0:3,0:3] = mat_3
+C_const = 3*np.identity(6) + mat_2
 b_min = 0.01
 b_max = 2
 bx_izq = np.linspace(b_min, b_max, int(Finura/2))
@@ -28,10 +28,12 @@ bz_der = np.linspace(b_max, b_min, int(Finura/2))
 count = np.array(range(Finura))
 bx = np.r_[bx_izq, bx_der]
 bz = np.r_[bz_izq, bz_der]
+vol = alpha*(bx**2)*bz
 b = np.c_[bx,bx,bz]
 filas = 15
 freqs = np.zeros((filas, Finura))
 for i in range(Finura):
+    m = (A**3)*rho*vol[i]
     gamma = rus.gamma_matrix(Ng, C_const, b[i,:], shape)
     E = rus.E_matrix(Ng, shape)
     vals, vects = scipy.linalg.eigh(a = (m**(-1/3))*gamma, b = E)
