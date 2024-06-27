7b0d8f8bfbdec58bc78a24cf8d2b311bb36ea7a1
jacubillos10 <alejandro4cm@outlook.com>
2024-06-26 21:14:07 -0500

    A new idea to adimensionalize the problem has been executed. the eigenvalue m^(2/3)(omega^2) does not depend of the size of the sample, only it's proportions. Take a look to the A variable in the main.py code, change it, of course the frequencies will change but m^(2/3)(omega^2) remains constant

--- main.py
+++ main.py
@@ -20,34 +20,35 @@ Ng = 16
 rho = 4.869 #g/cm^3
 nombre_archivo = 'constantesSmB6.csv' #Mbar
 """
-
+A = 1
 #Datos del URu2Si2
 Ng = 14
-m = 0.2634 #g 9.84029 #9.839 #g/cm^3 
+m = (A**3)*0.2634 #g 9.84029 #9.839 #g/cm^3 
 nombre_archivo = 'constantesURu2Si2.csv' #Mbar
 
 
 C_const = np.genfromtxt(nombre_archivo, delimiter=',', skip_header=0, dtype=float)
 #geometry = np.array([0.30529,0.20353,0.25334]) #cm  FeGa
 #geometry = np.array([0.10872, 0.13981, 0.01757]) #cm SmB6
-geometry = np.array([0.29605, 0.31034, 0.29138]) #cm URu2Si2
+geometry = A*np.array([0.29605, 0.31034, 0.29138]) #cm URu2Si2
 #Note que el doble factorial de 2 tiró un segfault. En algún momento puede estar pasando esto. 
 #print(rus.fact2(-1))
 gamma = rus.gamma_matrix(Ng, C_const, geometry, shape)
 E = rus.E_matrix(Ng, shape)
 
-vals, vects = scipy.linalg.eigh(a = gamma, b = E)
+vals, vects = scipy.linalg.eigh(a = (m**(-1/3))*gamma, b = E)
 
 print("Norma: ", np.linalg.norm(gamma - gamma.T))
 print("Norma: ", np.linalg.norm(E - E.T))
 print(vals[0])
 
-vals = (vals/m)**0.5
-vals_vueltas = vals*(1/(2*np.pi))
-np.savetxt(nombre_archivo[:-4] + '_freq_' + str(shape) +'.csv', np.c_[range(len(vals)),vals, vals_vueltas], delimiter = ',')
+freq = (vals/(m**(2/3)))**0.5
+freq_vueltas = freq*(1/(2*np.pi))
+np.savetxt(nombre_archivo[:-4] + '_freq_' + str(shape) +'.csv', np.c_[range(len(vals)), freq, freq_vueltas], delimiter = ',')
 
 print("N vals: ", len(vals))
-print(vals_vueltas[6:6+12])
+print(freq_vueltas[6:6+12])
+print(vals[6:6+12])
 
 #time.sleep(5)
 
