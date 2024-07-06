f29107694d6b02a6291ec48b5506ca636b6e33d5
jacubillos10 <alejandro4cm@outlook.com>
2024-07-05 22:30:29 -0500

    Fixed some mistakes in markdown files and minor mistakes in the files to build the project such setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         classifiers = [
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
         ],
         license = "MIT",
         python_requires = ">=3.6",
