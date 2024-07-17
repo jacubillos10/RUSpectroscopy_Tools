a48d14ce8d67c8c10b6de9fbf374a4e74a6aa20a
jacubillos10 <alejandro4cm@outlook.com>
2024-07-17 18:05:38 -0500

    Gaussian data generator has been added

--- rusmodules/data_generator.py
+++ rusmodules/data_generator.py
@@ -1,11 +1,6 @@
 import numpy as np
-from numba import njit, types
-from numba.pycc import CC
-cc = CC('data_generator')
 
-@njit("f8[:,:](f8,f8,i8)")
-@cc.export("generate_C_matrix", "f8[:,:](f8,f8,i8)")
-def generate_C_matrix(C_min, C_max, crystal_structure):
+def generate_C_matrix(C_min, C_max, crystal_structure, distribution = 0):
     C = np.zeros((6,6))
     C_prc = np.zeros(3)
     C_sec = np.zeros(6)
@@ -13,9 +8,13 @@ def generate_C_matrix(C_min, C_max, crystal_structure):
     d_struc = np.array([[3,6],[2,4],[1,2],[1,1]])
     n_p = d_struc[crystal_structure,0]
     n_s = d_struc[crystal_structure,1]
-    C_prc_raw = np.random.uniform(C_min, C_max, n_p)
-    C_max_sec = 0.5*min(C_prc)
-    C_sec_raw = np.random.uniform(C_min, C_max_sec, n_s)
+    if distribution == 1:
+        C_prc_raw = np.random.normal(C_min, C_max, n_p)
+        C_sec_raw = np.random.normal(0.5*C_min, 0.5*C_max, n_s)
+    else:
+        C_prc_raw = np.random.uniform(C_min, C_max, n_p)
+        C_max_sec = 0.5*min(C_prc_raw)
+        C_sec_raw = np.random.uniform(C_min, C_max_sec, n_s)
     if crystal_structure == 1:
         C_prc[0:2] = C_prc_raw[0]
         C_prc[2] = C_prc_raw[1]
