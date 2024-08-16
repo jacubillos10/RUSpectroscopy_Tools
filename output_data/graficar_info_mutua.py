64e13ba322201b14081fea54d9282d5e51f17a49
jacubillos10 <alejandro4cm@outlook.com>
2024-08-16 17:38:32 -0500

    A code to generate nice mutual information plots has been added

--- output_data/graficar_info_mutua.py
+++ output_data/graficar_info_mutua.py
@@ -8,28 +8,30 @@ targets = MI_nuevas.keys()
 features_nuevas = MI_nuevas.index
 features_antiguas = MI_antiguas.index
 N_datos = 30000
-print(MI_nuevas)
-print(targets)
-print(features_nuevas)
-print(features_antiguas)
 
-fig1 = plt.figure(figsize=(30,30))
+fig1 = plt.figure(figsize=(20,20))
 for i in range(len(targets)):
     num = int(str(33) + str(i+1))
     target = targets[i]
     ax = fig1.add_subplot(num)
     ax.barh(features_antiguas, MI_antiguas[target])
-    ax.set_title(target)
+    ax.set_title(target, fontsize=24)
+    ax.tick_params(axis='x', labelsize=21)  # Larger x-labels
+    ax.tick_params(axis='y', labelsize=14)  # Slightly larger y-labels
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
 plt.savefig("Omega_" + str(N_datos) + "datos.png")
 
 # %%
-fig2 = plt.figure(figsize = (30,30))
+fig2 = plt.figure(figsize = (20,20))
 for i in range(len(targets)):
     num = int(str(33) + str(i+1))
     target = targets[i]
     ax = fig2.add_subplot(num)
     ax.barh(features_nuevas, MI_nuevas[target])
-    ax.set_title(target)
+    ax.set_title(target, fontsize=24)
+    ax.tick_params(axis='x', labelsize=21)  # Larger x-labels
+    ax.tick_params(axis='y', labelsize=14)  # Slightly larger y-labels
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
 plt.savefig("Eigen_" + str(N_datos) + "datos.png")
 
 
